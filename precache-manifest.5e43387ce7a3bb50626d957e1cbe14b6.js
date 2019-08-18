7092479f7e5cd5d251e1cfaf19a4e1e77056df81
Vinoth <ecevino20@gmail.com>
2019-08-19 01:49:04 +0530

    Updates

---
+++ precache-manifest.5e43387ce7a3bb50626d957e1cbe14b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7be920320adbe9ff5bf3a2cf11ec5d",
+    "url": "/mail/index.html"
+  },
+  {
+    "revision": "3bd3359b32df2a0c82c3",
+    "url": "/mail/static/css/main.aa69f033.chunk.css"
+  },
+  {
+    "revision": "beca195845a9c40eb145",
+    "url": "/mail/static/js/2.081521c1.chunk.js"
+  },
+  {
+    "revision": "3bd3359b32df2a0c82c3",
+    "url": "/mail/static/js/main.2c0d0c2d.chunk.js"
+  },
+  {
+    "revision": "dddd0fdea3093673cbd4",
+    "url": "/mail/static/js/runtime~main.5cf8ed88.js"
+  }
+]);
